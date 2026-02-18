8963bd88da11f97f1aca9a94e49f7ccea1677f19
David Beneš <david@davidbenes.cz>
2026-02-18 22:50:34 +0100

    feat: Redirect to account page after successful registration or login, display the logged-in username, and set authentication cookies.

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -2,13 +2,20 @@ async function signup(username, password) {
     let logins = await getJson("data/logins.json")
     logins.push({ username, password })
     await saveJson("data/logins.json", logins)
+    window.location.href = "ucet.html"
 }
 
 async function login(username, password) {
     let logins = await getJson("data/logins.json")
     for (let i = 0; i < logins.length; i++) {
         if (logins[i].username === username && logins[i].password === password) {
-            alert("Přihlášen")
+            window.location.href = "ucet.html"
+
+            document.cookie = "username=" + username
+            document.cookie = "password=" + password
+            document.cookie = "isLoggedIn=" + true
+
+            document.getElementById("user").innerHTML = username
             return true
         }
     }
