8767c492cc427d76797f115102070f46c62dcf70
David Beneš <david@davidbenes.cz>
2026-02-18 20:39:42 +0100

    feat: Dynamically render shop items from data in `render.js` and remove hardcoded items from `index.html`.

--- assets/js/render.js
+++ assets/js/render.js
@@ -1,13 +1,16 @@
-// function renderItems() {
-//     let items = getItems()
-//     for (let i = 0; i < items.length; i++) {
-//         let item = items[i]
-//         let itemElement = document.createElement("div")
-//         itemElement.innerHTML = `
-//             <h2>${item.name}</h2>
-//             <p>${item.price}</p>
-//             <button onclick="addToCart(${item.id})">Přidat do košíku</button>
-//         `
-//         document.getElementById("items").appendChild(itemElement)
-//     }
-// }
+async function renderItems() {
+    let items = await getItems()
+    let shopElement = document.querySelector(".shop")
+    shopElement.innerHTML = ""
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i]
+        let itemElement = document.createElement("div")
+        itemElement.classList.add("item")
+        itemElement.innerHTML = `
+            <img src="${item.img}" alt="${item.name}" class="items">
+            <p>${item.price},-</p>
+            <button onclick="addToCart(${item.id})">DO KOŠÍKU</button>
+        `
+        shopElement.appendChild(itemElement)
+    }
+}
