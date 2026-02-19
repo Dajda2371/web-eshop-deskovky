93b82e8cc9335342326980ac007d92328d61baeb
David Beneš <david@davidbenes.cz>
2026-02-19 01:06:22 +0100

    feat: Implement cookie path and login state checking on page load, and remove password storage from cookies.

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -11,9 +11,8 @@ async function login(username, password) {
         if (logins[i].username === username && logins[i].password === password) {
             window.location.href = "ucet.html"
 
-            document.cookie = "username=" + username
-            document.cookie = "password=" + password
-            document.cookie = "isLoggedIn=" + true
+            document.cookie = "username=" + username + "; path=/"
+            document.cookie = "isLoggedIn=" + true + "; path=/"
 
             document.getElementById("user").innerHTML = username
             return true
@@ -25,7 +24,26 @@ async function login(username, password) {
 
 function logout() {
     document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-    document.cookie = "password=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
     document.cookie = "isLoggedIn=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
     window.location.href = "index.html"
 }
+
+function getCookie(name) { // najde cookie podle jmena a vrati jeji value (browser magie)
+    let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) return match[2];
+    return null;
+}
+
+function checkLoginState() { // podle tyhle funkce tam zobrazi ussername
+    let username = getCookie("username");
+    let userSpan = document.getElementById("user");
+    if (userSpan) {
+        if (username) {
+            userSpan.innerText = username;
+        } else {
+            userSpan.innerText = "Nepřihlášen";
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", checkLoginState); // az se ta stranka nacte tak se callne checkLoginState
